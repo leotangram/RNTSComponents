f4fc905eb2f4f01767152f0511f5350a956cf52b
Leonardo Omaña <leotangram@gmail.com>
2021-07-06 11:33:08 -0500

    Component flatlist

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,38 @@
 import React from 'react'
-import { View } from 'react-native'
-import Icon from 'react-native-vector-icons/Ionicons'
+import { FlatList, Text, View } from 'react-native'
+
+interface MenuItem {
+  component: string
+  icon: string
+  name: string
+}
+
+const menuItems: MenuItem[] = [
+  {
+    component: 'Animation101',
+    icon: 'cube-outline',
+    name: 'Animation 101'
+  }
+]
 
 const HomeScreen = () => {
+  const renderMenuItem = (menuItem: MenuItem) => {
+    return (
+      <View>
+        <Text>
+          {menuItem.name} - {menuItem.icon}
+        </Text>
+      </View>
+    )
+  }
+
   return (
-    <View>
-      <Icon color="#000000" name="star-outline" size={30} />
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={menuItems}
+        keyExtractor={item => item.name}
+        renderItem={({ item }) => renderMenuItem(item)}
+      />
     </View>
   )
 }
