d4e1b64dbe6a7fdccecb679c7de07384bb842c58
Leonardo Omaña <leotangram@gmail.com>
2021-07-06 11:20:34 -0500

    Stack & Icons

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Icon color="#000000" name="star-outline" size={30} />
+    </View>
+  )
+}
+
+export default HomeScreen
