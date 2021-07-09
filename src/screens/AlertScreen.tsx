edb2abb8472ac424b8bc12d347a474278b8f25c2
Leonardo Omaña <leotangram@gmail.com>
2021-07-09 10:42:23 -0500

    Prompt ios -  android

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Alert, Button, View } from 'react-native'
+import prompt from 'react-native-prompt-android'
 import HeaderTitle from '../components/HeaderTitle'
 import { styles } from '../theme/appTheme'
 
@@ -23,19 +24,41 @@ const AlertScreen = () => {
     )
 
   const showPrompt = () =>
-    Alert.prompt(
-      'Are you sure?',
-      'This actions in not reversible',
-      (value: string) => console.log('info: ', value),
-      'plain-text',
-      'Hi!',
-      'number-pad'
+    // Alert.prompt(
+    //   'Are you sure?',
+    //   'This actions in not reversible',
+    //   (value: string) => console.log('info: ', value),
+    //   'plain-text',
+    //   'Hi!',
+    //   'number-pad'
+    // )
+    prompt(
+      'Enter password',
+      'Enter your password to claim your $1.5B in lottery winnings',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel'
+        },
+        {
+          text: 'OK',
+          onPress: password => console.log('OK Pressed, password: ' + password)
+        }
+      ],
+      {
+        type: 'secure-text',
+        cancelable: false,
+        defaultValue: 'test',
+        placeholder: 'placeholder'
+      }
     )
 
   return (
     <View style={styles.globalMargin}>
       <HeaderTitle title="Alerts" />
       <Button onPress={showAlert} title="Show alert" />
+      <View style={{ height: 10 }} />
       <Button onPress={showPrompt} title="Show prompt" />
     </View>
   )
