740eefaf4c9c0b93cc5a5bad8d68504cf9d774c7
Leonardo Omaña <leotangram@gmail.com>
2021-07-09 11:05:33 -0500

    Many TextInputs

--- src/screens/TextInputScreen.tsx
+++ src/screens/TextInputScreen.tsx
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, TextInput, View } from 'react-native'
 import HeaderTitle from '../components/HeaderTitle'
 import { styles } from '../theme/appTheme'
 
 const TextInputScreen = () => {
+  const [form, setForm] = useState({
+    email: '',
+    name: '',
+    phone: ''
+  })
+
+  const { email, name, phone } = form
+
+  const onChange = (value: string, field: string) =>
+    setForm({
+      ...form,
+      [field]: value
+    })
+
   return (
     <View style={styles.globalMargin}>
       <HeaderTitle title="TextInputs" />
-      <TextInput style={stylesTextInput.inputStyle} />
+      <TextInput
+        autoCapitalize="words"
+        autoCorrect={false}
+        onChangeText={value => onChange(value, 'name')}
+        placeholder="Add your name"
+        style={stylesTextInput.inputStyle}
+      />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        keyboardAppearance="dark"
+        keyboardType="email-address"
+        onChangeText={value => onChange(value, 'email')}
+        placeholder="Add your email"
+        style={stylesTextInput.inputStyle}
+      />
+      <TextInput
+        keyboardType="phone-pad"
+        onChangeText={value => onChange(value, 'phone')}
+        placeholder="Add your phone"
+        style={stylesTextInput.inputStyle}
+      />
+      <HeaderTitle title={JSON.stringify(form, null, 3)} />
     </View>
   )
 }
@@ -20,6 +56,7 @@ const stylesTextInput = StyleSheet.create({
     borderRadius: 10,
     borderWidth: 1,
     height: 50,
+    marginVertical: 10,
     paddingHorizontal: 10
   }
 })
